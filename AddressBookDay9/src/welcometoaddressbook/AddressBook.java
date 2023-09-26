4ae2c63af3ea79eca8b1fbdeb6629b6e44685959
KadamTeju <tejushinde8897@gmail.com>
2023-09-26 12:19:27 +0530

    UC1 Complete.

--- AddressBookDay9/src/welcometoaddressbook/AddressBook.java
+++ AddressBookDay9/src/welcometoaddressbook/AddressBook.java
@@ -1,6 +1,63 @@
 package welcometoaddressbook;
 import java.util.ArrayList;
 class Contact{
- 
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int zipNo;
+    private int phoneNumber;
+    private String email;
+    private static ArrayList<Contact> list = new ArrayList<Contact>();
+
+
+    public Contact(String firstName, String lastName, String address, int zipNo, int phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.zipNo = zipNo;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getZipNo() {
+        return zipNo;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+public class AddressBook
+{
+    public static void main(String[] args)
+    {
+
+
+        System.out.printf("Hello and welcome!");
+
+
+        for (int i = 1; i <= 5; i++)
+        {
+
+
+            System.out.println("i = " + i);
+        }
+    }
 
 }
